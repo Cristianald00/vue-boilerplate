953263be9171ea39c131257279df6125c10cbe28
Cristian <cristianald00@hotmail.com>
2022-03-09 17:15:41 -0500

    Initial Branch

---
+++ src/main.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import '@/plugins/axios'
+import vuetify from '@/plugins/vuetify'
+import '@/plugins/veevalidate'
+import '@/plugins/common'
+import '@/plugins/googleAnalytics'
+import i18n from '@/plugins/i18n'
+import App from '@/App.vue'
+import router from '@/router'
+import { store } from '@/store'
+import VuetifyConfirm from 'vuetify-confirm'
+/* Font Awesome */
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHatWizard } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// Add icon libraries to be used
+library.add(faHatWizard)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
+Vue.use(VuetifyConfirm, { vuetify })
+
+const app = new Vue({
+    vuetify,
+    router,
+    store,
+    i18n,
+    render: (h) => h(App),
+    created() {
+        store.dispatch('setLocale', store.getters.locale)
+        if (store.getters.isTokenSet) {
+            store.dispatch('autoLogin')
+        }
+    }
+}).$mount('#app')
+
+if (window.Cypress) {
+    // Only available during E2E tests
+    window.app = app
+}
---
+++ src/modules/dashboard/routes.js
@@ -0,0 +1,13 @@
+export default [
+    {
+        path: '/dashboard',
+        name: 'dashboard',
+        meta: {
+            requiresAuth: true
+        },
+        component: () =>
+            import(
+                /* webpackChunkName: "dashboard" */ './components/Dashboard.vue'
+            )
+    }
+]
